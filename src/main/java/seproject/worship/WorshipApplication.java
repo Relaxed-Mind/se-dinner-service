a8534111fa54f784d33b6b00781978880afb2d8e
slowcom <dlwlstn0709@uos.ac.kr>
2022-09-21 09:43:28 +0900

    스프링부트 초기 연결

---
+++ src/main/java/seproject/worship/WorshipApplication.java
@@ -0,0 +1,13 @@
+package seproject.worship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seproject/worship/WorshipApplicationTests.java
@@ -0,0 +1,13 @@
+package seproject.worship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
