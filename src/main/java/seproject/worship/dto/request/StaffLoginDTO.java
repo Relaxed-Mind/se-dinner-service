f01601c83beb7bff73d3493692262f1d352ecb9f
Lee sang min <dkwkghkdlxld2@gmail.com>
2022-11-05 18:29:15 +0900

    staff login 구현

---
+++ src/main/java/seproject/worship/dto/request/StaffLoginDTO.java
@@ -0,0 +1,10 @@
+package seproject.worship.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class StaffLoginDTO {
+    private String sid;
+    private String pw;
+}
