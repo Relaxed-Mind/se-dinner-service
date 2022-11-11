5ffc8d2c019fa61e5423562ea38068a9a1a8d285
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2022-11-11 09:48:58 +0900

    고객 장바구니 속 메뉴 목록 조회 API

--- src/main/java/seproject/worship/dto/response/CartLoadMenuListDTO.java
+++ src/main/java/seproject/worship/dto/response/CartLoadMenuListDTO.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import seproject.worship.entity.CartMenu;
 import seproject.worship.entity.Menu;
+import seproject.worship.enumpack.StyleStatus;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,12 +15,14 @@ public class CartLoadMenuListDTO {
     private String menuURL;
     private Integer count;
     private Integer cartMenuPrice;
+    private String styleStatus;
 
     public CartLoadMenuListDTO(Menu menu, CartMenu cartMenu){
         this.menuName = menu.getName();
         this.menuURL = menu.getMenuUrl();
         this.count = cartMenu.getCount();
         this.cartMenuPrice = cartMenu.getCartMenuPrice();
+        this.styleStatus = cartMenu.getStyleStatus().name();
     }
 
     public Map<String, Object> CartLoadMenuListDTOtoMap(){
@@ -28,6 +31,7 @@ public class CartLoadMenuListDTO {
         map.put("menuURL", this.menuURL);
         map.put("count", this.count);
         map.put("cartMenuPrice", this.cartMenuPrice);
+        map.put("styleStatus", this.styleStatus);
 
         return map;
     }
