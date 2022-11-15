d9d4657d9bcc5d456665d63c52005d30ba0b72b7
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2022-11-15 20:38:27 +0900

    고객 재주문 API + 주문조회 orderService에 위임

--- src/main/java/seproject/worship/controller/CustomerController.java
+++ src/main/java/seproject/worship/controller/CustomerController.java
@@ -6,43 +6,52 @@ import seproject.worship.dto.request.CustomerLoginDTO;
 import seproject.worship.dto.request.CustomerModifyInfoDTO;
 import seproject.worship.dto.request.CustomerSaveDTO;
 import seproject.worship.dto.response.CustomerLoadInfoDTO;
+import seproject.worship.service.CartService;
 import seproject.worship.service.CustomerService;
+import seproject.worship.service.OrderService;
 
 import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/customer")
+
 public class CustomerController {
     private final CustomerService customerService;
+    private final CartService cartService;
+    private final OrderService orderService;
 
-    @PostMapping
+    @PostMapping("/customer")
     public Map customerSave(@RequestBody CustomerSaveDTO dto){
         return customerService.customerSave(dto);
     }
 
-    @PostMapping("/login")
+    @PostMapping("/customer/login")
     public Map customerLogin(@RequestBody CustomerLoginDTO dto) { return customerService.customerLogin(dto);}
 
-    @GetMapping("/{customerId}")
+    @GetMapping("/customer/{customerId}")
     public CustomerLoadInfoDTO customerLoadInfo(@PathVariable Long customerId){
         return customerService.customerLoadInfo(customerId);
     }
 
-    @PutMapping()
+    @PutMapping("/customer")
     public Map customerModifyInfo(@RequestBody CustomerModifyInfoDTO dto){
         return customerService.customerModifyInfo(dto);
     }
 
-    @GetMapping("/{customerId}/orders")
+    @GetMapping("/customer/{customerId}/orders")
     public Map customerLoadOrderList(@PathVariable Long customerId){
-        return customerService.customerLoadOrderList(customerId);
+        return orderService.customerLoadOrderList(customerId);
     }
 
-    @GetMapping("/{customerId}/orders/{orderId}")
-    public Map customerViewSpecificOrder(@PathVariable Long customerId,
-                                         @PathVariable Long orderId) {
-        return customerService.customerViewSpecificOrder(customerId, orderId);
+    @GetMapping("/orders/{orderId}")
+    public Map customerViewSpecificOrder(@PathVariable Long orderId) {
+        return orderService.customerViewSpecificOrder(orderId);
+    }
+
+    @GetMapping("/customer/{customerId}/reorder/{orderId}")
+    public Map customerReorder(@PathVariable Long customerId
+                               ,@PathVariable Long orderId){
+        return cartService.customerReorder(customerId, orderId);
     }
 
 }
