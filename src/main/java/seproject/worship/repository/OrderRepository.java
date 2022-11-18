b2488f04917334b9d8e1997efffb82fcfdb5c7d6
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2022-11-18 11:26:39 +0900

    직원 주문 거절

--- src/main/java/seproject/worship/repository/OrderRepository.java
+++ src/main/java/seproject/worship/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package seproject.worship.repository;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import seproject.worship.entity.Order;
 import seproject.worship.entity.OrderMenu;
 import seproject.worship.enumpack.OrderStatus;
@@ -8,7 +10,7 @@ import seproject.worship.enumpack.OrderStatus;
 import java.util.Collection;
 import java.util.List;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
 
     List<Order> findByOrderStatus(OrderStatus orderStatus);
     List<Order> findAllByOrderStatusIn(List<OrderStatus> orderStatuses);
