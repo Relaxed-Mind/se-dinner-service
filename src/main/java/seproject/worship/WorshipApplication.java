abad69cff8c0a25df057d0719b8e8b72e1eb6b24
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2022-11-23 16:20:45 +0900

    직원 주문 거절

--- src/main/java/seproject/worship/WorshipApplication.java
+++ src/main/java/seproject/worship/WorshipApplication.java
@@ -2,13 +2,10 @@ package seproject.worship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import seproject.worship.speech.FileUploadProperties;
+
+
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-		FileUploadProperties.class
-})
 public class WorshipApplication {
 
 	public static void main(String[] args) {
--- src/main/java/seproject/worship/dto/request/ItemAddListDTO.java
+++ src/main/java/seproject/worship/dto/request/ItemAddListDTO.java
@@ -5,5 +5,5 @@ import lombok.Getter;
 import java.util.List;
 @Getter
 public class ItemAddListDTO {
-    List<ItemAddDTO> results;
+    List<ItemAddDTO> Results;
 }
