ff9d418e6da71dc1d88645745b49aa4a2b8b602c
Lee-Sang-Min1998 <dkwkghkdlxld2@gmail.com>
2022-11-23 16:03:28 +0900

    직원 주문 거절

--- src/main/java/seproject/worship/WorshipApplication.java
+++ src/main/java/seproject/worship/WorshipApplication.java
@@ -2,8 +2,13 @@ package seproject.worship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import seproject.worship.speech.FileUploadProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileUploadProperties.class
+})
 public class WorshipApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/seproject/worship/speech/FileUploadProperties.java
@@ -0,0 +1,19 @@
+package seproject.worship.speech;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix="file")
+@Component
+public class FileUploadProperties {
+    private String uploadDir;
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
+}
+
